153eeca262d25dc03121f4f0a8b9a500cfddd497
LIU, CHAO-WEI <100680093+chaowei-dev@users.noreply.github.com>
2025-07-17 02:31:29 +0800

    Time: 3 ms (39.22%) | Memory: 41.5 MB (92.33%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+
+        for (char c : s.toCharArray()) {
+            // put all open to stack
+            if (map.containsKey(c)) {
+                if (!stack.isEmpty() && stack.peek() == map.get(c)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
